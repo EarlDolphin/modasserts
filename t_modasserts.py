d8eba53238cd1f51cacb26bdf9ca9716531c5121
EarlDolphin <calvin.kosterman@gmail.com>
2015-01-08 12:35:54 -0700

    experimenting with lambdas

--- t_modasserts.py
+++ t_modasserts.py
@@ -2,41 +2,40 @@
 import unittest
 from modasserts import myAsserts
 
-def func(x): return 1/x
 
 class TestTheTests(unittest.TestCase, myAsserts):
   def setup(self):
-    pass
+    self.func = (lambda x: 1/x)
 
   def test_pass1(self):
     self.assertFail(asrt=self.assertEqual, first=1, second=2)
   def test_pass2(self):
-    self.assertFail(asrt=func, x=0)
+    self.assertFail(asrt=self.func, x=0)
   def test_pass3(self):
-    self.assertFail(asrt=self.assertRaises, excClass=Exception, callableObj=func, x=5)
+    self.assertFail(asrt=self.assertRaises, excClass=Exception, callableObj=self.func, x=5)
   def test_pass4(self):
     self.assertFail(self.assertEqual, first=1, second=2)
   def test_pass5(self):
-    self.assertFail(func, x=0)
+    self.assertFail(self.func, x=0)
   def test_pass6(self):
-    self.assertFail(self.assertRaises, excClass=Exception, callableObj=func, x=5)
+    self.assertFail(self.assertRaises, excClass=Exception, callableObj=self.func, x=5)
   def test_pass7(self):
     self.assertFail(self.assertEqual, 1, 2)
   def test_pass8(self):
-    self.assertFail(func, 0)
+    self.assertFail(self.func, 0)
   def test_pass9(self):
-    self.assertFail(self.assertRaises, Exception, func, 5)
+    self.assertFail(self.assertRaises, Exception, self.func, 5)
 
   def test_fail1(self):
     try: self.assertFail(asrt=self.assertEqual, first=1, second=1)
     except AssertionError: pass
     except Exception as e: raise e
   def test_fail2(self):
-    try: self.assertFail(asrt=func, x=5)
+    try: self.assertFail(asrt=self.func, x=5)
     except AssertionError: pass
     except Exception as e: raise e
   def test_fail3(self):
-    try: self.assertFail(asrt=self.assertRaises, excClass=ZeroDivisionError, callableObj=func, x=0)
+    try: self.assertFail(asrt=self.assertRaises, excClass=ZeroDivisionError, callableObj=self.func, x=0)
     except AssertionError: pass
     except Exception as e: raise e
   def test_fail4(self):
@@ -44,11 +43,11 @@ class TestTheTests(unittest.TestCase, myAsserts):
     except AssertionError: pass
     except Exception as e: raise e
   def test_fail5(self):
-    try: self.assertFail(func, x=5)
+    try: self.assertFail(self.func, x=5)
     except AssertionError: pass
     except Exception as e: raise e
   def test_fail6(self):
-    try: self.assertFail(self.assertRaises, excClass=ZeroDivisionError, callableObj=func, x=0)
+    try: self.assertFail(self.assertRaises, excClass=ZeroDivisionError, callableObj=self.func, x=0)
     except AssertionError: pass
     except Exception as e: raise e
   def test_fail7(self):
@@ -56,11 +55,11 @@ class TestTheTests(unittest.TestCase, myAsserts):
     except AssertionError: pass
     except Exception as e: raise e
   def test_fail8(self):
-    try: self.assertFail(func, 5)
+    try: self.assertFail(self.func, 5)
     except AssertionError: pass
     except Exception as e: raise e
   def test_fail9(self):
-    try: self.assertFail(self.assertRaises, ZeroDivisionError, func, 0)
+    try: self.assertFail(self.assertRaises, ZeroDivisionError, self.func, 0)
     except AssertionError: pass
     except Exception as e: raise e
 
