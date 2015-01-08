ad2eae02db384d4d113af02891ec4ee8c9e56e3e
EarlDolphin <calvin.kosterman@gmail.com>
2015-01-08 12:19:30 -0700

    added docstring

--- modasserts.py
+++ modasserts.py
@@ -1,6 +1,10 @@
 #!/usr/bin/env python3
 class myAsserts:
   def assertFail(self, asrt, *args, **kwargs):
+    """
+    Test a test to ensure it fails.
+    <asrt> :unittest.TestCase.assert*
+    """
     try: asrt(*args, **kwargs)
     except: pass
     else: raise AssertionError("Hey! This test was supposed to fail!")
